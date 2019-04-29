e6aaf628537f88c99388576c05d6d3d8aaa06ac9
constantinos.koutsakis@gmail.com <constantinos.koutsakis@gmail.com>
2019-04-29 15:29:48 +1000

    Adding tests for input and output validation

--- WaterOverflow/MyTest.py
+++ WaterOverflow/MyTest.py
@@ -17,8 +17,8 @@ class Test_MyTest(unittest.TestCase):
     # Data type validation on parameters: we need to integers (i, j) and a float (K)
     # The order of parameters is conventional, assuming they appear as ints first and the float after
     # Therefore, the expected order of parameters is (int i, int j, float K)
+    # There can be an additional test to check if the function returns a float
     def test_types_of_input_variables(self):
-        ii = 11;
         sig = inspect.signature(WaterOverflow.calculate_liquid)
         params = sig.parameters
         if 'i' in params and 'j' in params and 'K' in params:
@@ -29,6 +29,31 @@ class Test_MyTest(unittest.TestCase):
             if iParam.annotation is int and jParam.annotation is int and KParam.annotation == float:
                 areParamsCorrectType = True
         self.assertTrue(doKeysExist and areParamsCorrectType)
+
+    # Data validation for j: it has to be lesser or equal than i
+    # If a user inputs a j larger than i, it should use j = i (expected functionality - can be amended if the requirement is different)
+    # There is an error in the glass demonstration diagram in the exercise, for i = 3: j = 0, j = 1, j = 1, j = 2
+    # Should read: i = 3: j = 0, j = 1, j = 2, j = 3
+    def test_j_lesser_or_equal_than_i(self):
+        i = 5;
+        j = 6;
+        K = 2.0;
+        self.assertTrue(j > i)
+        
+        waterJGreaterThanI = WaterOverflow.calculate_liquid(i, j, K)
+        waterJEqualToI = WaterOverflow.calculate_liquid(i, i, K)
+
+        # Initially this test passes if the function returns K
+        self.assertTrue(waterJGreaterThanI == waterJEqualToI)
+
+    # The first test for the body: Each glass can hold 250ml
+    # The output of the function must not exceed 0.25
+    def test_glass_max_volume(self):
+        i = 5;
+        j = 6;
+        K = 2.0;
+        water = WaterOverflow.calculate_liquid(i, j, K)
+        self.assertLessEqual(0.25, water)
 
 if __name__ == '__main__':
     unittest.main()
--- WaterOverflow/WaterOverflow.py
+++ WaterOverflow/WaterOverflow.py
@@ -4,8 +4,15 @@ import unittest
 import inspect
 
 # Calculates the volume of liquid in glass j of row i 
-def calculate_liquid(i: int, j: float, K: float):
-    return K
+def calculate_liquid(i: int, j: int, K: float):
+    #code for test_j_lesser_or_equal_than_i
+    if (j > i):
+        j = i
+    #code for test_glass_max_volume
+    if(K < 0.25):
+        return K
+    else:
+        return 0.25
 
 # The main to use the function
 if __name__ == "__main__":
